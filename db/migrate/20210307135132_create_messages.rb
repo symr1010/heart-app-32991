dbe8f5b959092eee7273def3a90bd4c29596b5fc
symr1010 <bgzlud@gmail.com>
2021-10-17 08:13:56 +0900

    messagesテーブルの作成

--- db/migrate/20210307135132_create_messages.rb
+++ db/migrate/20210307135132_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.string  :content
-      t.references :room, foreign_key: true
-      t.references :user, foreign_key: true
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
